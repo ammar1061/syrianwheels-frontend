f4311ed7ba5c2450a967fd3513243fde027ba85e
ammar1061 <ammareteish.sy@gmail.com>
2025-05-19 10:53:43 +0200

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -6,13 +6,6 @@ import theme from './theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import CarsPage from './pages/CarsPage';
-import CarDetailsPage from './pages/CarDetailsPage';
-import BookingPage from './pages/BookingPage';
-import UserDashboard from './pages/UserDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -24,13 +17,6 @@ function App() {
         <main style={{ minHeight: 'calc(100vh - 120px)', padding: '20px 0' }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/cars" element={<CarsPage />} />
-            <Route path="/cars/:id" element={<CarDetailsPage />} />
-            <Route path="/booking/:id" element={<BookingPage />} />
-            <Route path="/dashboard" element={<UserDashboard />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
