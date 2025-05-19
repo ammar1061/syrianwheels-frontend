e4f9d50995f2ff6bb57d55042515fd5ef75e898c
ammar1061 <ammareteish.sy@gmail.com>
2025-05-19 10:45:35 +0200

    Create theme.js

---
+++ src/src/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  direction: 'rtl',
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
+export default theme;
